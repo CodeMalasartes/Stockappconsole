33f721f10c6f6930d3d3904add4304d2bf2667e4
Ricardo Rodrigues <r.jorge.rodrigues@hotmail.com>
2021-01-16 11:33:59 +0000

    Add project files.

---
+++ UdemyExe/Produto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace UdemyExe
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValotTotalEmStock()
+        {
+            return Preco * Quantidade;
+        }
+
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            //Quantidade = Quantidade + quantidade;
+            Quantidade += quantidade;
+
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            //Quantidade = Quantidade - quantidade;
+            Quantidade -= quantidade;
+
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValotTotalEmStock().ToString("F2", CultureInfo.InvariantCulture);
+                
+        }
+    }
+}
---
+++ UdemyExe/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace UdemyExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no stock: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto:" + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o nº de produtos a ser adicionados ao stock: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atuais: " + p);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o nº de produtos a ser removidos ao stock: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atuais: " + p);
+        }
+    }
+}
